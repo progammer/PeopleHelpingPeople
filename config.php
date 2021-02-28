d3c05adb1c50b26433facc150f8d7819c629b874
Radster859 <31903875+Radster859@users.noreply.github.com>
2021-02-28 07:15:35 -0600

    Works?

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 server with default setting (user 'root' with no password) */
 
 /* TO BE REPLACED BY GOOGLE CLOUD SQL DETAILS */
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', '34.71.184.13');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+define('DB_PASSWORD', 'a7Nkqe7euZ46nvb_!*EC');
+define('DB_NAME', 'hackutddatabase');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT provID, username, password FROM provider WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
